5aa7e416c17112090535881ad65d9cccab6ab7e1
Emre Şahiner <emre@workintech.com.tr>
2024-10-08 11:03:17 +0300

    add new post action added for testing purposes

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,10 +1,19 @@
+import { useDispatch } from "react-redux";
 import Posts from "../components/Posts";
+import { addNewPost } from "../store/actions/postActions";
+import { postData } from "../data";
 
 function Feed() {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    const post = postData[Math.floor(Math.random() * postData.length)];
+    dispatch(addNewPost(post));
+  };
   return (
     <div>
       <h1>Home</h1>
-      <div>Post burada</div>
+      <div onClick={handleClick}>Post burada</div>
       <Posts />
     </div>
   );
